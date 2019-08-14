c95cc65a0e98127988e08e6a84aeed3775150e9e
Brooks Patton <brooks@brookzerker.com>
2019-08-14 08:17:33 -0600

    creating comments in the neural network file

--- deep_learning/neural_network.js
+++ deep_learning/neural_network.js
@@ -2,11 +2,8 @@ const {
     relu,
     dotVectorMatrix,
     createRandomMatrix,
-    vectorSubtract,
     transpose,
-    vectorMultiply,
     reluToDerivative,
-    outerProduct,
     scalarMatrixMultiply,
     matrixSubtract,
     matrixMultiply,
@@ -15,19 +12,27 @@ const {
 
 function neuralNetwork(data) {
     const { trainingData, testingData } = data;
+    // We want to create an alpha value that will be used to limit the learning of the network. This will prevent the network from over learning and wildly spinning out of control.
     let alpha = 0.0001;
+    // We are using two neurons here. The first will feed into the second neuron. The first predictions are called the hidden layer. We need to choose how many predictions we want the first neuron to make
     const hiddenSize = trainingData.inputs[0].length + 1;
+    // Time to generate the weights. We'll be using random weights to begin with because we have no idea what a good guess would be. The weights need to be a matrix with the number of rows set to the number of input data we are using for training. The columns will be equal to the number of predictions that we want our neuron to make
     let layer1Weights = createRandomMatrix(trainingData.inputs[0].length, hiddenSize);
+    // We have two neuron, so we need to create the weights for the second one as well. This neuron will be using the predictions from the first neuron as inputs, so its rows will be set to the hidden layer size. Also in this case we are guessing one thing, so we only need a single column for our final prediction
     let layer2Weights = createRandomMatrix(hiddenSize, 1);
-    let layer2Errors = 0;
-    const maxTrainingIterations = 1000000;
+    // We're training over a set of input data many times. For each of these iterations we are also looping over ever input. We are setting this so that we don't accidentally create a infinite loop that never ends
+    const maxTrainingIterations = 100000;
 
+    // Time to begin! Lets start by creating a loop that will repeat for every set of input data
     for (let training = 0; training < maxTrainingIterations; training = training + 1) {
-        if (training % 1000 === 0) previousErrors = layer2Errors;
+        // We want to know how our network is doing over all of the input data so lets create a final prediction error variable to capture that
+        let layer2Errors = 0;
 
-        layer2Errors = 0;
-
+        // We are looping over all of the input data we have, and for each of them we are running the inputs through the neural network
         trainingData.inputs.forEach((currentLayer0, index) => {
+            // A neuron prediction is simply getting the weighted sum (or dot) of the input vector (also known as an array for us in JavaScript land) and the weights. 
+
+            // We are going to take the prediction and turn off the negative values. This is because those negative values cause the end result to be way off. If we set them to 0, then they won't cause the rest of the neural network to get crazy.
             const layer1 = relu(dotVectorMatrix(currentLayer0, layer1Weights));
             const layer2 = dotVectorMatrix(layer1, layer2Weights);
 
